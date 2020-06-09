0affca67355b7e9db9dbc11e002e16019352d8c4
setgoogle <setisdale@google.com>
2020-06-09 19:15:30 +0000

    add google map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -43,6 +43,12 @@ function deleteData() {
     });
 }
 
+function createMap() {
+  const map = new google.maps.Map(
+  document.getElementById('map'),
+  {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 
 
 

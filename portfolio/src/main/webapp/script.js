e7a04a01078f8c5f718799ccf9dbf71fca6afb3f
setgoogle <setisdale@google.com>
2020-06-12 20:45:57 +0000

    added loop for marker listener" ;

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,86 +16,66 @@
  * Adds a random greeting to the page.
  */
 function addRandomFact() {
-  const facts =
-      ['My favorite animals are sloths', 'I had a phase where I was obssesed with Mayan mythology', 'My favorite anime is one piece', 'I love lasagna'];
+    const facts =
+        ['My favorite animals are sloths', 'I had a phase where I was obssesed with Mayan mythology', 'My favorite anime is one piece', 'I love lasagna'];
 
-  // Pick a random greeting.
-  const fact = facts[Math.floor(Math.random() * facts.length)];
+    // Pick a random greeting.
+    const fact = facts[Math.floor(Math.random() * facts.length)];
 
-  // Add it to the page.
-  const factContainer = document.getElementById('fact-container');
-  factContainer.innerText = fact;
+    // Add it to the page.
+    const factContainer = document.getElementById('fact-container');
+    factContainer.innerText = fact;
 }
 
 // recieves messages to be presented to the user
 function addMessage() {
-  var numComment = document.getElementById("numInput").value;
-  fetch('/data?numComments=' + numComment).then(response => response.text())
-  .then((message) => {
-  document.getElementById('message-container').innerHTML = message;
-  });
-}
-
-function deleteData() {
-  const promise = fetch(new Request('/delete-data', {method: 'POST'}));
-  promise.then(() => {
-  addComments();
+    var numComment = document.getElementById("numInput").value;
+    fetch('/data?numComments=' + numComment).then(response => response.text())
+    .then((message) => {
+    document.getElementById('message-container').innerHTML = message;
     });
 }
 
-// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDjhk-U7NK6nQwmqSScCuZvO3l3_Q88Kps';
-script.defer = true;
-script.async = true;
-
-// Attach your callback function to the `window` object
-window.initMap = function() {
-  // JS API is loaded and available
-};
-
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
+function deleteData() {
+    const promise = fetch(new Request('/delete-data', {method: 'POST'}));
+    promise.then(() => {
+    addComments();
+    });
+}
 
 function createMap() {
-    document.head.appendChild(script);
   const map = new google.maps.Map(
   document.getElementById('map'),
-  {center: {lat: 35.2271, lng: -80.8431}, zoom: 10});
+    {center: {lat: 35.2271, lng: -80.8431}, zoom: 10});
 
-const trexMarker = new google.maps.Marker({
-    position: {lat: 35.235661, lng: -80.903183},
-    map: map,
-    title: 'My High School'
-  });
+    var features = [
+      trexMarker = new google.maps.Marker({
+          position: {lat: 35.235661, lng: -80.903183},
+          map: map,
+          title: 'My High School'
+  }),
 
-const hibachiSpot = new google.maps.Marker({
-    position: {lat: 35.190220, lng: -80.921910},
-    map: map,
-    title: 'This where I had lunch last week'
-  });
+      hibachiSpot = new google.maps.Marker({
+          position: {lat: 35.190220, lng: -80.921910},
+          map: map,
+          title: 'This where I had lunch last week'
+  }),
 
-  const bossyBeulah = new google.maps.Marker({
-    position: {lat: 35.233580, lng: -80.876500},
-    map: map,
-    title: 'Best place to have chicken sandwiches in Charlotte also conviently 5 mins near me XD'
-  });
-
-    trexMarker.addListener('click', function() {
-          map.setZoom(15);
-          map.setCenter(trexMarker.getPosition());
+      bossyBeulah = new google.maps.Marker({
+          position: {lat: 35.233580, lng: -80.876500},
+          map: map,
+          title: 'Best place to have chicken sandwiches in Charlotte also conviently 5 mins near me XD'
+  }),
+];
+  // add a listener to each marker when clicked
+  for(var i = 0; i < features.length;i++) {
+      (function(x) {
+        features[i].addListener('click', function() {
+            map.setZoom(15);
+            console.log(x);
+            map.setCenter(features[x].getPosition());
         });
-
-
-    hibachiSpot.addListener('click', function() {
-          map.setZoom(15);
-          map.setCenter(hibachiSpot.getPosition());
-        });
-
-
-    bossyBeulah.addListener('click', function() {
-          map.setZoom(15);
-          map.setCenter(bossyBeulah.getPosition());
-        });
-      }
+       })(i)
+  }
+}
 
